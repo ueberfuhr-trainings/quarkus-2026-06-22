c5d1df6c29f4c77ab52079f12ba795fdcc9e524f
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-06-22 12:53:47 +0200

    Remove GreetingResource and associated tests from customer-api-provider

--- customer-api-provider/src/test/java/de/schulung/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package de.schulung;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
