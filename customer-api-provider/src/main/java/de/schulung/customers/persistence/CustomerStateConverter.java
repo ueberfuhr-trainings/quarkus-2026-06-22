fcfe195a1ed395c4179d66ae1698303dae128a8c
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-06-25 12:35:44 +0200

    Add JPA converter for CustomerState enumeration

---
+++ customer-api-provider/src/main/java/de/schulung/customers/persistence/CustomerStateConverter.java
@@ -0,0 +1,32 @@
+package de.schulung.customers.persistence;
+
+import de.schulung.customers.domain.CustomerState;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import jakarta.ws.rs.BadRequestException;
+
+
+@Converter(autoApply = true)
+public class CustomerStateConverter
+  implements AttributeConverter<CustomerState, String> {
+
+  @Override
+  public String convertToDatabaseColumn(CustomerState state) {
+    return null == state ? null : switch (state) {
+      case ACTIVE -> "active";
+      case LOCKED -> "locked";
+      case DISABLED -> "disabled";
+    };
+  }
+
+  @Override
+  public CustomerState convertToEntityAttribute(String state) {
+    return null == state ? null : switch (state) {
+      case "active" -> CustomerState.ACTIVE;
+      case "locked" -> CustomerState.LOCKED;
+      case "disabled" -> CustomerState.DISABLED;
+      default -> throw new BadRequestException();
+    };
+  }
+
+}
