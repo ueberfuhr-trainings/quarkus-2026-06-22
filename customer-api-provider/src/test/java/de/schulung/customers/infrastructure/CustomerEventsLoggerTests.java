f44a5bdea5f3d62e416e6349ef8ed9f604407b71
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-06-26 11:25:05 +0200

    Add test utilities and logging for customer events

--- customer-api-provider/src/test/java/de/schulung/customers/infrastructure/CustomerEventsLoggerTests.java
+++ customer-api-provider/src/test/java/de/schulung/customers/infrastructure/CustomerEventsLoggerTests.java
@@ -8,8 +8,6 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
@@ -19,30 +17,28 @@ class CustomersEventsLoggerTests {
 
   @Test
   void whenCreateCustomer_thenLogEvent(CapturedOutput output) {
-    var response = given()
+    final var newCustomerUuid = given()
       .contentType(ContentType.JSON)
       .body("""
-            {
-              "name": "Tom Mayer",
-              "birthdate": "2001-04-23",
-              "state": "active"
-            }
+        {
+          "name": "Tom Mayer",
+          "birthdate": "2006-06-23",
+          "state": "active"
+        }
         """)
       .accept(ContentType.JSON)
       .when()
-      .post("/customers");
-    response
+      .post("/customers")
       .then()
       .statusCode(201)
-      .header("Location", is(notNullValue()));
-    var uuid = response.body().jsonPath().getString("uuid");
+      .extract().path("uuid");
 
     // async: use Awaitility
     assertTrue(
-      output.toString().matches("(?si).*Customer created.*" + uuid + ".*"),
+      output.toString().matches("(?si).*Customer created.*" + newCustomerUuid + ".*"),
       """
         Expected log output to contain 'Customer created' with id "%s"
-        """.formatted(uuid)
+        """.formatted(newCustomerUuid)
     );
 
   }
